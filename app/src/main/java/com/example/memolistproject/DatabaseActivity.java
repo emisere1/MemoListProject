e7612c5f3a6518eed2e77e260884434dad7176a3
Christopher R <crodriguez39@student.gsu.edu>
2025-03-28 23:38:23 -0400

    added xml, inital db records, added icons, added date selection java and xml

--- app/src/main/java/com/example/memolistproject/DatabaseActivity.java
+++ app/src/main/java/com/example/memolistproject/DatabaseActivity.java
@@ -1,4 +1,26 @@
 package com.example.memolistproject;
-
-public class DatabaseActivity {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+public class DatabaseActivity extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MemoListProject.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_TABLE_MEMO_ENTRY = "create table memo_entry (_id integer primary key autoincrement, "
+            + "memotitle text not null, memodescription text, memodate text, memotext text);"; // initial memo db records
+    public DatabaseActivity(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_MEMO_ENTRY);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(DatabaseActivity.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS memo_entry");
+        onCreate(db);
+    }
 }
