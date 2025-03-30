3bb4ada7fb7c26526ca3e487930f88e3b292e864
ethan <emisere93@gmail.com>
2025-03-29 22:13:30 -0400

    MemoList xml+start of code

---
+++ app/src/main/java/com/example/memolistproject/MemoList.java
@@ -0,0 +1,42 @@
+package com.example.memolistproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MemoList extends AppCompatActivity {
+
+    ArrayList<Memo> memos;
+    RecyclerView memoList;
+
+    private View.OnClickListener onItemClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) view.getTag();
+            int position = viewHolder.getAdapterPosition();
+            int memoID = memos.get(position).getMemoID();
+            Intent intent = new Intent(MemoList.this, MemoEntryActivity.class);
+            intent.putExtra("memoID", memoID);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_memo_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
